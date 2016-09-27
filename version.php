c9ce4bacad03816a7db838b8e5d6eeea0e6250e5
valery <valery.fremaux@gmail.com>
2016-09-28 01:28:45 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -14,12 +14,20 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * @package    local_cms
+ * @category   local
+ * @author     Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright  2009 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016010801;   // The (date) version of this plugin
-$plugin->requires = 2015050500;   // Requires this Moodle version
-$plugin->release = '2.9.0 (Build 2015100600)';   // Requires this Moodle version
-$plugin->component = 'local_my';   // Requires this Moodle version
+$plugin->version  = 2016010801;   // The (date) version of this plugin.
+$plugin->requires = 2015050500;   // Requires this Moodle version.
+$plugin->release = '2.9.0 (Build 2015100600)';
+$plugin->component = 'local_my';
 
-// Non moodle attributes
+// Non moodle attributes.
 $plugin->codeincrement = '2.9.0001';
