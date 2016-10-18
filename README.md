58494e4474e059a6c7b67860d4da1663f4a8b31e
valery <valery.fremaux@gmail.com>
2016-10-18 17:27:53 +0200

    Automated sync from SVN

--- README.md
+++
@@ -1,2 +0,0 @@
-# moodle-local_my
-Alternative of moodle my pages using a modular assembly approach
--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2016010801;   // The (date) version of this plugin.
-$plugin->requires = 2016051900;   // Requires this Moodle version.
-$plugin->release = '3.1.0 (Build 2016010801)';
+$plugin->requires = 2014110400;   // Requires this Moodle version.
+$plugin->release = '2.8.0 (Build 2015100600)';
 $plugin->component = 'local_my';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '2.8.0001';
