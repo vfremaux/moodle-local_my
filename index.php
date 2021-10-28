77edac01eb682740565443fd5149e6ac5d312ce8
Valery Fremaux <valery.fremaux@gmail.com>
2021-10-28 18:15:51 +0200

    Automated sync from SVN

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,62 +15,159 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Lists the course categories
+ * My Moodle -- a user's personal dashboard
  *
- * @copyright 1999 Martin Dougiamas  http://dougiamas.com
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @package course
+ * - each user can currently have their own page (cloned from system and then customised)
+ * - only the user can see their own dashboard
+ * - users can add any blocks they want
+ * - the administrators can define a default site dashboard for users who have
+ *   not created their own dashboard
+ *
+ * This script implements the user's view of the dashboard, and allows editing
+ * of the dashboard.
+ *
+ * @package    moodlecore
+ * @subpackage my
+ * @copyright  2010 Remote-Learner.net
+ * @author     Hubert Chathi <hubert@remote-learner.net>
+ * @author     Olav Jordan <olav.jordan@remote-learner.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once("../config.php");
-require_once($CFG->dirroot. '/course/lib.php');
+require_once(__DIR__ . '/../config.php');
+require_once($CFG->dirroot . '/my/lib.php');
 
-$categoryid = optional_param('categoryid', 0, PARAM_INT); // Category id
-$site = get_site();
+redirect_if_major_upgrade_required();
 
-if ($CFG->forcelogin) {
-    require_login();
+// TODO Add sesskey check to edit
+$edit   = optional_param('edit', null, PARAM_BOOL);    // Turn editing on and off
+$reset  = optional_param('reset', null, PARAM_BOOL);
+
+require_login();
+
+$hassiteconfig = has_capability('moodle/site:config', context_system::instance());
+if ($hassiteconfig && moodle_needs_upgrading()) {
+    redirect(new moodle_url('/admin/index.php'));
 }
 
-$heading = $site->fullname;
-if ($categoryid) {
-    $category = core_course_category::get($categoryid); // This will validate access.
-    $PAGE->set_category_by_id($categoryid);
-    $PAGE->set_url(new moodle_url('/course/index.php', array('categoryid' => $categoryid)));
-    $PAGE->set_pagetype('course-index-category');
-    $heading = $category->get_formatted_name();
-} else if ($category = core_course_category::user_top()) {
-    // Check if there is only one top-level category, if so use that.
-    $categoryid = $category->id;
-    $PAGE->set_url('/course/index.php');
-    if ($category->is_uservisible() && $categoryid) {
-        $PAGE->set_category_by_id($categoryid);
-        $PAGE->set_context($category->get_context());
-        if (!core_course_category::is_simple_site()) {
-            $PAGE->set_url(new moodle_url('/course/index.php', array('categoryid' => $categoryid)));
-            $heading = $category->get_formatted_name();
-        }
-    } else {
-        $PAGE->set_context(context_system::instance());
+$strmymoodle = get_string('myhome');
+
+if (isguestuser()) {  // Force them to see system default, no editing allowed
+    // If guests are not allowed my moodle, send them to front page.
+    if (empty($CFG->allowguestmymoodle)) {
+        redirect(new moodle_url('/', array('redirect' => 0)));
     }
-    $PAGE->set_pagetype('course-index-category');
-} else {
-    throw new moodle_exception('cannotviewcategory');
+
+    $userid = null;
+    $USER->editing = $edit = 0;  // Just in case
+    $context = context_system::instance();
+    $PAGE->set_blocks_editing_capability('moodle/my:configsyspages');  // unlikely :)
+    $header = "$SITE->shortname: $strmymoodle (GUEST)";
+    $pagetitle = $header;
+
+} else {        // We are trying to view or edit our own My Moodle page
+    $userid = $USER->id;  // Owner of the page
+    $context = context_user::instance($USER->id);
+    $PAGE->set_blocks_editing_capability('moodle/my:manageblocks');
+    $header = fullname($USER);
+    $pagetitle = $strmymoodle;
 }
 
-$PAGE->set_pagelayout('coursecategory');
-$courserenderer = $PAGE->get_renderer('core', 'course');
+// Get the My Moodle page info.  Should always return something unless the database is broken.
+if (!$currentpage = my_get_page($userid, MY_PAGE_PRIVATE)) {
+    print_error('mymoodlesetup');
+}
 
-$PAGE->set_heading($heading);
-$content = $courserenderer->course_category($categoryid);
+// Start setting up the page
+$params = array();
+$PAGE->set_context($context);
+$PAGE->set_url('/my/index.php', $params);
+$PAGE->set_pagelayout('mydashboard');
+$PAGE->set_pagetype('my-index');
+$PAGE->blocks->add_region('content');
+$PAGE->set_subpage($currentpage->id);
+$PAGE->set_title($pagetitle);
+$PAGE->set_heading($header);
+
+if (!isguestuser()) {   // Skip default home page for guests
+    if (get_home_page() != HOMEPAGE_MY) {
+        if (optional_param('setdefaulthome', false, PARAM_BOOL)) {
+            set_user_preference('user_home_page_preference', HOMEPAGE_MY);
+        } else if (!empty($CFG->defaulthomepage) && $CFG->defaulthomepage == HOMEPAGE_USER) {
+            $frontpagenode = $PAGE->settingsnav->add(get_string('frontpagesettings'), null, navigation_node::TYPE_SETTING, null);
+            $frontpagenode->force_open();
+            $frontpagenode->add(get_string('makethismyhome'), new moodle_url('/my/', array('setdefaulthome' => true)),
+                    navigation_node::TYPE_SETTING);
+        }
+    }
+}
+
+// Toggle the editing state and switches
+if (empty($CFG->forcedefaultmymoodle) && $PAGE->user_allowed_editing()) {
+    if ($reset !== null) {
+        if (!is_null($userid)) {
+            require_sesskey();
+            if (!$currentpage = my_reset_page($userid, MY_PAGE_PRIVATE)) {
+                print_error('reseterror', 'my');
+            }
+            redirect(new moodle_url('/my'));
+        }
+    } else if ($edit !== null) {             // Editing state was specified
+        $USER->editing = $edit;       // Change editing state
+    } else {                          // Editing state is in session
+        if ($currentpage->userid) {   // It's a page we can edit, so load from session
+            if (!empty($USER->editing)) {
+                $edit = 1;
+            } else {
+                $edit = 0;
+            }
+        } else {
+            // For the page to display properly with the user context header the page blocks need to
+            // be copied over to the user context.
+            if (!$currentpage = my_copy_page($USER->id, MY_PAGE_PRIVATE)) {
+                print_error('mymoodlesetup');
+            }
+            $context = context_user::instance($USER->id);
+            $PAGE->set_context($context);
+            $PAGE->set_subpage($currentpage->id);
+            // It's a system page and they are not allowed to edit system pages
+            $USER->editing = $edit = 0;          // Disable editing completely, just to be safe
+        }
+    }
+
+    // Add button for editing page
+    $params = array('edit' => !$edit);
+
+    $resetbutton = '';
+    $resetstring = get_string('resetpage', 'my');
+    $reseturl = new moodle_url("$CFG->wwwroot/my/index.php", array('edit' => 1, 'reset' => 1));
+
+    if (!$currentpage->userid) {
+        // viewing a system page -- let the user customise it
+        $editstring = get_string('updatemymoodleon');
+        $params['edit'] = 1;
+    } else if (empty($edit)) {
+        $editstring = get_string('updatemymoodleon');
+    } else {
+        $editstring = get_string('updatemymoodleoff');
+        $resetbutton = $OUTPUT->single_button($reseturl, $resetstring);
+    }
+
+    $url = new moodle_url("$CFG->wwwroot/my/index.php", $params);
+    $button = $OUTPUT->single_button($url, $editstring);
+    $PAGE->set_button($resetbutton . $button);
+
+} else {
+    $USER->editing = $edit = 0;
+}
 
 echo $OUTPUT->header();
-echo $OUTPUT->skip_link_target();
-echo $content;
 
-// Trigger event, course category viewed.
-$eventparams = array('context' => $PAGE->context, 'objectid' => $categoryid);
-$event = \core\event\course_category_viewed::create($eventparams);
-$event->trigger();
+echo $OUTPUT->custom_block_region('content');
 
 echo $OUTPUT->footer();
+
+// Trigger dashboard has been viewed event.
+$eventparams = array('context' => $context);
+$event = \core\event\dashboard_viewed::create($eventparams);
+$event->trigger();
