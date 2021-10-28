15ef237efd3277e7f3e63b2f0ce3cbf7cdebe35d
Valery Fremaux <valery.fremaux@gmail.com>
2021-10-28 18:11:37 +0200

    Automated sync from SVN

--- localprolib.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*   __________________________________________________
-    |              on 2.0.12              |
-    |__________________________________________________|
-*/
- namespace local_my; defined("\115\117\117\104\114\x45\137\111\x4e\124\105\122\116\101\114") || die; final class local_pro_manager { private static $component = "\154\157\143\x61\154\x5f\155\171"; private static $componentpath = "\x6c\157\x63\x61\x6c\57\155\171"; public static function add_settings(&$fgUIw, &$n1aas) { global $CFG, $PAGE; } }
