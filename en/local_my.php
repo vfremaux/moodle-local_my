0c3f168e3dd04972291cb4d1e4c9e4fbea99f59f
Valery Fremaux <valery.fremaux@gmail.com>
2021-10-28 18:10:50 +0200

    Automated sync from SVN

---
+++ en/local_my.php
@@ -0,0 +1,336 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['my:overridemy'] = 'Can override My routing';
+$string['my:ismanager'] = 'Has manager activity in site';
+$string['my:iscoursemanager'] = 'Has manager activity in courses';
+$string['my:isstudent'] = 'Has student activity in some course';
+$string['my:isteacher'] = 'Has teacher activity in some courses';
+$string['my:isauthor'] = 'Has editing teacher activity in some courses';
+$string['my:seecourseattributes'] = 'See course attributes (enrol, visibility, editability)';
+
+// Privacy.
+$string['privacy:metadata'] = 'The Local My plugin does not directly store any personal data about any user.';
+
+$string['ariaviewselectoroption'] = '{$a} sort option';
+$string['ariaviewfilteroption'] = '{$a} filter option';
+$string['ariaviewtimeoption'] = '{$a} schedule option';
+$string['addnewsubcategory'] = 'Add new subcategory';
+$string['addtofavorites'] = 'Add to favorites';
+$string['adminview'] = '<span class="tinytext">This content is only visible if user profile {$a->field} is "{$a->value}</span>"';
+$string['allcategories'] = 'All categories';
+$string['allcourses'] = 'Courses';
+$string['asadmin'] = 'Administrator';
+$string['asadminpagetitle'] = 'Administrator';
+$string['ascoursemanager'] = 'Course manager';
+$string['ascoursemanagerpagetitle'] = 'Course manager';
+$string['assignmentstosubmit'] = 'Assignments';
+$string['asstudent'] = 'Student';
+$string['asstudentpagetitle'] = 'Student';
+$string['asteacher'] = 'Teacher';
+$string['asteacherpagetitle'] = 'Teacher';
+$string['available'] = 'Available';
+$string['availablecourses'] = 'Available courses for free enrol';
+$string['backtohome'] = 'Back to home page';
+$string['block'] = 'Block';
+$string['byname'] = 'By name';
+$string['byenddate'] = 'By end date';
+$string['bycompletion'] = 'By completion';
+$string['bylastaccess'] = 'Most recent first';
+$string['byfavorites'] = 'By preference';
+$string['cachedef_heatmap'] = 'User\'s heatmap data';
+$string['canedit'] = "Can edit";
+$string['caneditcourse'] = 'Course is editable';
+$string['categories'] = 'Categories';
+$string['categorysettings'] = 'Category Settings';
+$string['choosecategory'] = 'Choose a category for creating course';
+$string['choosecoursetoenrollin'] = 'Choose a course to enrol in';
+$string['collapseall'] = 'Collapse all';
+$string['completedusers'] = 'Completed users';
+$string['completion'] = 'Completion: {$a}%';
+$string['connectedusers'] = 'Connected (once) accounts';
+$string['contentfor'] = 'Content for people having \'{$a->profile}\' being \'{$a->data}\'';
+$string['courseareas'] = 'Course areas';
+$string['courseareasavailables'] = 'Course areas availables';
+$string['courseareasandavailables'] = 'Course areas with availables';
+$string['courseareasettings'] = 'Course Areas Settings';
+$string['coursecompletionratio'] = 'Course completion ratio';
+$string['coursecreation'] = 'Course creation';
+$string['courselistssettings'] = 'Course list accessory settings';
+$string['coursesearch'] = 'Search courses';
+$string['defaultleftcolumntext'] = '';
+$string['editing'] = 'You can edit this course';
+$string['effecthalo'] = "Halo effect";
+$string['effectopacity'] = "Opacity effect";
+$string['enabledusers'] = 'Active accounts';
+$string['enrollablecourses'] = 'Courses you can self enrol in';
+$string['enrolled'] = 'Enrolled students';
+$string['errorbadblock'] = 'Bad block instance {$a} (maybe not in context)';
+$string['expandall'] = 'Expand all';
+$string['everything'] = 'Everything';
+$string['fieldnotfound'] = 'The field {$a} was not found';
+$string['filestorage'] = 'File storage size';
+$string['filterisediting'] = 'Has editing role';
+$string['fullpath'] = 'Full uppath';
+$string['cannotedit'] = 'Has no editing role';
+$string['canedit'] = 'Has editing role';
+$string['coursename'] = 'Course name';
+$string['displayauto'] = 'Auto display';
+$string['displaycards'] = 'Cards';
+$string['displaylist'] = 'List';
+$string['displaysummary'] = 'Summary';
+$string['filled'] = '{count} {name} on {date}';
+$string['frequentationitem'] = "page";
+$string['frequentationitemplural'] = "pages";
+$string['future'] = "Future course (not opened)";
+$string['futurecourses'] = 'Future courses';
+$string['guestenrol'] = "Open to guests";
+$string['heatmapsettings'] = 'Heatmap Settings';
+$string['inner'] = "Between {down} and {up}";
+$string['ishidden'] = "Is hidden";
+$string['latestnews'] = 'Latest news';
+$string['lefteditioncolumn'] = 'Left edition column';
+$string['localcourseboxheight'] = 'Course box height';
+$string['localmyadddetailindicators'] = 'Add detail indicators';
+$string['localmyadddetailindicators_desc'] = '';
+$string['localmyadminmodules'] = 'Admin panel modules';
+$string['localmycoursearea'] = 'Course area';
+$string['localmycourseareas'] = 'Specific course areas';
+$string['localmycourselistaccordion'] = 'Use accordion behaviour of course lists';
+$string['localmycourselistaccordion_desc'] = 'Use accordion behaviour of course lists';
+$string['localmycoursemanagermodules'] = 'Course manager panel modules';
+$string['localmyenable'] = 'Enable customized my';
+$string['locallightfavorites'] = 'Enable light favorites';
+$string['locallightfavorites_desc'] = 'Uses the light favorite system in course lists and sorting by preference.';
+$string['categoryareasettings'] = 'Course categories areas settings';
+$string['localmycategoryareas'] = 'Course category areas';
+$string['localmycategoryarea'] = 'Category area course categories';
+$string['localmyenablerolecontrolincourseareas'] = 'Enable role check in course areas';
+$string['localmyenablerolecontrolincourseareas_desc'] = 'If set, the coursearea list is filtered by the panel role. Non matching courses will not appear in this area instance (in this panel).';
+$string['localmyexcludedcourses'] = 'Course to exclude';
+$string['localmyforce'] = 'Force my page to users';
+$string['localmyheatmaprange'] = 'HeatMap Range';
+$string['localmyhidedescriptions'] = 'Hide descriptions';
+$string['localmyhidedescriptions_desc'] = 'Do not display course summaries in course boxes';
+$string['localmyhideprogression'] = 'Hide progression';
+$string['localmyhideprogression_desc'] = 'Do not display progress gauges or signals';
+$string['localmylayout'] = 'Local my layout';
+$string['localmymaxavailablelistsize'] = 'Max size of available courses';
+$string['localmymaxoverviewedlistsize'] = 'Max size of course list with full overviewes';
+$string['localmymaxuncategorizedlistsize'] = 'Max size of course list with mention of category';
+$string['localmymodules'] = 'Student panel modules ';
+$string['localmyprintcategories'] = 'Print with categories (1 level)';
+$string['localmyprogressgaugeheight'] = 'Progress bar height';
+$string['localmyprogressgaugeheight_desc'] = '';
+$string['localmyprogressgaugetype'] = 'Progress bar type';
+$string['localmyprogressgaugetype_desc'] = '';
+$string['localmyprogressgaugewidth'] = 'Progress bar width';
+$string['localmyprogressgaugewidth_desc'] = '';
+$string['localmyshowfilterstates'] = 'show filter states';
+$string['localmyshowfilterstates_desc'] = 'If enabled, the state of all activated filters will be explicitely shown on screen';
+$string['localmyshowcourseidentifier'] = 'Show course identifier';
+$string['localmyshowcourseidentifier_desc'] = 'If enabled, adds the course identifier to the course list output';
+$string['localmyslick'] = 'Enable slider';
+$string['localmyslick_desc'] = 'If disabled, will display blocsk in grid without sliding track.';
+$string['localmyteachermodules'] = 'Teacher panel modules ';
+$string['localmytrimlength1'] = 'Trim length 1';
+$string['localmytrimlength1_desc'] = 'Trim length units for short strings';
+$string['localmytrimlength2'] = 'Trim length 2';
+$string['localmytrimlength2_desc'] = 'Trim length units for descriptions or summaries texts (note that trim do remove all tags)';
+$string['localmytrimmode'] = 'Trim mode';
+$string['localmytrimmode_desc'] = 'The algorithm used to trim texts';
+$string['localmyuselefteditioncolumn'] = 'Use left editorial column';
+$string['localprofilefieldforcegraphicmode'] = 'Profile field for forcing graphic mode';
+$string['localprofilefieldforcegraphicmode_desc'] = '';
+$string['localprofilefieldforcegraphicvalues'] = 'Profile values for forcing graphic mode';
+$string['localprofilefieldforcegraphicvalues_desc'] = '';
+$string['localprofilefieldforcelistmode'] = 'Profile field for forcing list mode';
+$string['localprofilefieldforcelistmode_desc'] = '';
+$string['localprofilefieldforcelistvalues'] = 'Profile values for forcing list mode';
+$string['localprofilefieldforcelistvalues_desc'] = '';
+$string['localskipmymetas'] = 'Skip my metacourses';
+$string['localmydefaultcoursedisplayoption'] = 'Default display mode for course lists';
+$string['localmydefaultcoursedisplayoption_desc'] = '';
+$string['localmydefaultcoursetimeoption'] = 'Default time option for course lists';
+$string['localmydefaultcoursetimeoption_desc'] = '';
+$string['localmydefaultcoursesortoption'] = 'Default sort options for course lists';
+$string['localmydefaultcoursesortoption_desc'] = '';
+$string['localmywithdisplay'] = 'Enable course list manual display control';
+$string['localmywithsort'] = 'Enable course list sorting';
+$string['localmywithtimeselector'] = 'Enable course list time selector';
+$string['localmywithdisplay_desc'] = '';
+$string['localmywithsort_desc'] = '';
+$string['localmywithtimeselector_desc'] = '';
+$string['localmyaddcourseindexlink'] = 'Add tab linked to global course index';
+$string['localmyaddcourseindexlink_desc'] = '';
+$string['localmycategorypathstopcats'] = 'Up path stop categories';
+$string['localmycategorypathstopcats_desc'] = 'When displaying full path of categories, you may give here a list of "path stop" categories ids to stop the up path discovery. Path will be stopped "under" the mentionned ids.';
+$string['localmyacceptfullpathrootcats'] = 'Accept full paths under categories';
+$string['localmyacceptfullpathrootcats_desc'] = 'If defined, gives a list of upper roots where to display full paths. Other tree branches will only display the current category. If empty, all categories display full path if required.';
+$string['courseindex'] = 'Course index';
+$string['lower'] = "Less than {min} {name}";
+$string['uppercat'] = "With upper category";
+$string['managemycourses'] = 'Manage my courses';
+$string['me'] = 'Me';
+$string['myactivity'] = 'My activity';
+$string['myauthoringcourses'] = 'My authoring courses';
+$string['mycalendar'] = 'My calendar';
+$string['mycategories'] = 'My authoring categories';
+$string['mycoursecompletion'] = 'My completion ratio';
+$string['mycourses'] = 'My courses';
+$string['myfavoritecourses'] = 'My favorites';
+$string['mymanagedcourses'] = 'Courses i manage';
+$string['mynetwork'] = 'My network';
+$string['myteachercourses'] = 'My courses as a teacher';
+$string['myteachings'] = 'My teaching';
+$string['mytemplates'] = 'My templates';
+$string['newcourse'] = 'Create new course';
+$string['newcoursefromtemplate'] = 'Create course from template';
+$string['newtemplate'] = 'New template';
+$string['noavailablecourses'] = 'No course in free access.';
+$string['nocontent'] = 'You have no content reachable in this site.';
+$string['nocourseareas'] = 'No course areas';
+$string['nocourses'] = 'No courses.';
+$string['none'] = '(None)';
+$string['noprogress'] = 'No progress';
+$string['nostaticguitexts'] = 'You are requiring the {$a} dashboard module but local_staticguitexts is not installed in your system. Install it or remove the entry in the module lists.';
+$string['notavailable'] = 'Not available module';
+$string['notavailablemodule'] = 'the required dashboard module {$a} is NOT available. It may not be part of your distribution.';
+$string['notcompiledyet'] = 'Stats were never compiled yet. you can wait the next stats task run, or <a href="{$a}">force compile now (it may take some time)</a>';
+$string['notrim'] = '(None)';
+$string['numberoffiles'] = 'Number of files';
+$string['onlineusers'] = 'Online users (5 min.)';
+$string['opencourses'] = 'Oen courses';
+$string['pendingassignstosubmit'] = 'Pending assigns to submit';
+$string['pluginname'] = 'Enhanced my';
+$string['progressbar'] = 'Bar';
+$string['progressdonut'] = 'Donut';
+$string['progressgauge'] = 'Gauge';
+$string['quiztoperform'] = 'Quiz to answer';
+$string['recentcourses'] = 'Most Recent Courses ';
+$string['removefromfavorites'] = 'Remove from favorites';
+$string['rendererimages'] = 'Renderer images';
+$string['restorecourse'] = 'Restore a course';
+$string['seealllist'] = '... see more courses.';
+$string['seeallnews'] = 'See all news...';
+$string['selfenrol'] = "Self enrollable";
+$string['sitestats'] = 'Site stats';
+$string['standardcreation'] = 'Create a new empty course';
+$string['static'] = 'Static texts';
+$string['staticguitextsnotinstalled'] = 'You need installing the <a href="https://github.com/vfremaux/moodle-local_staticguitexts">local_staticguitexts</a> plugin for this widget being used.';
+$string['summary'] = 'Summary';
+$string['suspendedusers'] = 'Suspended accounts';
+$string['task_compile_stats'] = 'Site wide stats';
+$string['templateinitialisationadvice'] = 'No templates. Administrator should create first template before all other users can create their own.';
+$string['tocompleteusers'] = 'Users yet having to complete';
+$string['todisplayauto'] = 'Switch to Auto display';
+$string['todisplaycards'] = 'Switch to Cards display';
+$string['todisplaylist'] = 'Switch to List display';
+$string['todisplaysummary'] = 'Switch to Summary display';
+$string['trimchars'] = "Chars";
+$string['trimwords'] = "Words";
+$string['unattemptedquizusers'] = 'Students with unattempted quizs';
+$string['uncompletequizusers'] = 'Users with quiz yet to do';
+$string['uncompletequizusersratio'] = 'Quiz completion ratio (users)';
+$string['uncompleteassignusers'] = 'Users with pending assigns to submit';
+$string['unknownmodule'] = 'Unknown output module {$a}';
+$string['upper'] = "More than {max} {name}";
+$string['unset'] = "-- Unset --";
+$string['youaredisplaying'] = 'You are displaying';
+$string['visualsettings'] = "Visual settings";
+$string['all'] = 'All';
+$string['passed'] = 'Passed courses';
+$string['current'] = 'Current courses';
+$string['future'] = 'Future courses';
+$string['hidden'] = 'Hidden courses';
+
+$string['january'] = 'January';
+$string['february'] = 'February';
+$string['march'] = 'March';
+$string['april'] = 'April';
+$string['may'] = 'May';
+$string['june'] = 'June';
+$string['july'] = 'July';
+$string['august'] = 'August';
+$string['september'] = 'September';
+$string['october'] = 'October';
+$string['november'] = 'November';
+$string['december'] = 'December';
+
+$string['localmymodules_desc'] = '
+<br><p>Page modules can be assembled to build the full dashboard view. Mention any module in order
+(one per line. Adding an "-L" suffix will force the module in the left column stack
+if dual stack is enabled by adding the "left_edition_column" module in the list.</p>
+<p>Default modules:</p>
+<li>me: short user identity block</li>
+<li>my_courses</li>
+<li>my_templates</li>
+<li>authored_courses</li>
+<li>available_courses</li>
+<li>course_areas: special dedicated course areas</li>
+<li>course_areas2: special dedicated course areas (second instance)</li>
+<p>Other modules can be used:</p>
+<li>latestnews_full</li>
+<li>latestnews_headers</li>
+<li>my_network: user\'s accessible network</li>
+<li>fullme: complete identity block</li>
+<li>my_calendar</li>
+<li>recent_courses</li>
+<li>my_templates (needs local plugin course_templates)</li>
+<li>static<n></li>
+';
+
+$string['localmyteachermodules_desc'] = '
+An optional module set that adds separate panel for teachers.
+';
+
+$string['localmyadminmodules_desc'] = '
+An optional module set that adds separate panel for admins.
+';
+
+$string['localmycoursemanagermodules_desc'] = '
+An optional module set that adds separate panel for course managers.
+';
+
+$string['rendererimages_desc'] = 'All images for renderer. We expect a "coursedefaultimage" image';
+
+$string['localmycourseareas_desc'] = 'Specific course areas are courses lists picked into a specific head category';
+
+$string['localmyenable_desc'] = 'If activated, the customized my replaces standard my page';
+
+$string['localmyforce_desc'] = 'If enabled, users will be forced to My page, unless they have myoverride capability
+(this is related to a block navigation hack. See README)';
+
+$string['localmyheatmaprange_desc'] = 'Range (in months) of the heatmap';
+
+$string['localmymaxavailablelistsize_desc'] = 'Sets the max number of available courses that will be printed. Set 0
+to full disable list size limit';
+
+$string['localmymaxoverviewedlistsize_desc'] = 'Sets the max number of courses that will be printed with full activity overview.
+Set 0 to full disable overviewes';
+
+$string['localmymaxuncategorizedlistsize_desc'] = 'Over this course count, courses will be shown with mention of category';
+
+$string['localmyprintcategories_desc'] = 'Print with categories (1 level) if enabld';
+
+$string['localmyuselefteditioncolumn_desc'] = 'If enabled, prints a left editorial column for admins to put text or institutional
+communication inside';
+
+$string['localskipmymetas_desc'] = 'Skip metacourses in my if enabled';
+
+$string['localmyexcludedcourses_desc'] = 'Enter a list of course ids';
+
+include(__DIR__.'/pro_additional_strings.php');
